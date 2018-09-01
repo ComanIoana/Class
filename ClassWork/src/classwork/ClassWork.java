f82c23ab3cd4a1490face41fae044f3995ed81c3
Coman Ioana <ComanIoana@users.noreply.github.com>
2018-09-01 18:10:00 +0300

    My first program

---
+++ ClassWork/src/classwork/ClassWork.java
@@ -0,0 +1,19 @@
+/*
+ * First 
+ */
+package classwork;
+
+/**
+ *
+ * @author ajcpa
+ */
+public class ClassWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Salut distinsi colegi!");
+    }
+    
+}
